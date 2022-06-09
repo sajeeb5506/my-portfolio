55737e692001d31978a569b21c6b8b25cb9eea36
sajeeb5506 <sajeebsarker5506@gmail.com>
2022-06-09 12:02:45 +0600

    monponents setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { Routes, Route} from "react-router-dom";
+import Home from "./components/Pages/Home/Home";
+import About from "./components/Pages/Home/About";
+import Projects from './components/Pages/Home/Projects';
+import Contact from './components/Pages/Home/Contact';
+import Navber from "./components/Pages/Home/Navber";
 
 function App() {
   return (
     <div className="">
-      <h1>sajeeb my portfolio done</h1>
+     <Navber/>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="contact" element={<Contact />} />
+        </Routes>
     </div>
   );
-}
+}      
 
 export default App;
---
+++ src/components/Pages/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import About from './About';
+import Banner from './Banner';
+import Contact from './Contact';
+import Projects from './Projects';
+
+const Home = () => {
+    return (
+        <div>
+            <Banner/>
+            <About></About>
+            <Projects></Projects>
+            <Contact></Contact>
+            
+        </div>
+    );
+};
+
+export default Home;
