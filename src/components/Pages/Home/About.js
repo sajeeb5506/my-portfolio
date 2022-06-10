d44dbaa67c9847f9ad6794f1adf2672da5e5b65c
sajeeb5506 <sajeebsarker5506@gmail.com>
2022-06-10 23:34:25 +0600

    about page edit

--- src/components/Pages/Home/About.js
+++ src/components/Pages/Home/About.js
@@ -2,49 +2,79 @@ import React from 'react';
 import './About.css'
 const About = () => {
     return (
+<div className="px-6">
+<div class="hero min-h-screen " id='about'>
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <img src="https://i.ibb.co/4PDW95p/rsz-1sajeeb.jpg" class="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 class="text-5xl font-bold text-indigo-200">About Me!!</h1>
+      <p class="py-6 text-orange-600 font-bold">I am MD.Sajeeb Sarker. I am a front-end web developer. I also make the website  interactive with web animations.
+      I can provide clean code and  perfect design. 
+      A responsive design makes your website accessible to all users, regardless of their device.
+
+       </p >
+       
+       
+       <p className='text-orange-600 py-6 font-bold mb-5'>
+
+       I am a Full Stack WEB Developer. I am working with React, Node.js,Express JS, React Native and MongoDB.
+       I also have experience in developing static websites using HTML, CSS and JavaScript(ES6).
+       </p>
+       <a 
+                    target='_blank'
+                      href="https://drive.google.com/file/d/1c3-J2y-kfj2fymMziB0Zo1CMkfrcn2Hc/view"
+                      className=" resume-btn about-btn "
+                    >
+                      Get Resume
+                    </a>
+    </div>
+  </div>
+</div>
+</div>
       
-        <div class="  px-8 " id='about'>
-           <h1 className='text-4xl font-extrabold tracking-tight text-orange-600 text-center'>About Me</h1>
-           <div className="flex items-center justify-between about-div ">
+      //   <div class="  px-8 " id='about'>
+      //      <h1 className='text-4xl font-extrabold tracking-tight text-orange-600 text-center'>About Me</h1>
+      //      <div className="flex items-center justify-between about-div ">
                
-                   <div className="">
-                <h1 className='text-4xl tracking-tight font-extrabold text-indigo-200  mb-5'>Education</h1>
-                  <h2 className='text-3xl font-sans font-bold text-indigo-200' >SSC 2017(science)</h2>
-                  <p className='text-2xl font-serif font-bold text-orange-600 mb-5'>Sobuj School Kanan & college </p>
-                  <h2 className='text-3xl font-sans font-bold text-indigo-200'>HSC 2019(science)</h2>
-                  <p className='text-2xl font-serif font-bold text-orange-600 mb-5'>Islamia Govt College</p>
-                  <h2 className='text-3xl font-sans font-bold text-indigo-200'>BBA 2019-20 sessoin(Physics Department)</h2>
-                  <p className='text-2xl font-serif font-bold text-orange-600 mb-5'>National University, Gazipur , Bangladesh</p>
-                  <h2 className='text-3xl font-sans font-bold text-indigo-200'>Programming</h2>
-                  <p className='text-2xl font-serif font-bold text-orange-600 mb-5'>Programming Hero , Youtube , Google</p>
-                  </div>
+      //              <div className="">
+      //           <h1 className='text-4xl tracking-tight font-extrabold text-indigo-200  mb-5'>Education</h1>
+      //             <h2 className='text-3xl font-sans font-bold text-indigo-200' >SSC 2017(science)</h2>
+      //             <p className='text-2xl font-serif font-bold text-orange-600 mb-5'>Sobuj School Kanan & college </p>
+      //             <h2 className='text-3xl font-sans font-bold text-indigo-200'>HSC 2019(science)</h2>
+      //             <p className='text-2xl font-serif font-bold text-orange-600 mb-5'>Islamia Govt College</p>
+      //             <h2 className='text-3xl font-sans font-bold text-indigo-200'>BBA 2019-20 sessoin(Physics Department)</h2>
+      //             <p className='text-2xl font-serif font-bold text-orange-600 mb-5'>National University, Gazipur , Bangladesh</p>
+      //             <h2 className='text-3xl font-sans font-bold text-indigo-200'>Programming</h2>
+      //             <p className='text-2xl font-serif font-bold text-orange-600 mb-5'>Programming Hero , Youtube , Google</p>
+      //             </div>
                
              
-                   <div className="">
-              <h1  className='text-4xl tracking-tight font-extrabold text-indigo-200  mb-5'> Programming Skill</h1>
-              <h2 className='text-2xl font-serif font-bold text-orange-600 '>html</h2>
-              <p><progress class="progress progress-success w-56" value="90" max="100"></progress>   </p>
-              <h2 className='text-2xl font-serif font-bold text-orange-600'>Css</h2>
-              <p><progress class="progress progress-success w-56" value="70" max="100"></progress></p>
-              <h2 className='text-2xl font-serif font-bold text-orange-600'>JavaScript</h2>
-              <p><progress class="progress progress-success w-56" value="65" max="100"></progress></p>
-              <h2 className='text-2xl font-serif font-bold text-orange-600'>React</h2>
-              <p><progress class="progress progress-success w-56" value="70" max="100"></progress></p>
-              <h2 className='text-2xl font-serif font-bold text-orange-600'>Node Js</h2>
-              <p><progress class="progress progress-success w-56" value="60" max="100"></progress></p>
-              <h2 className='text-2xl font-serif font-bold text-orange-600'>MongoDb</h2>
-              <p><progress class="progress progress-success w-56" value="65" max="100"></progress></p>
-              <h2 className='text-2xl font-serif font-bold text-orange-600'>Firebase</h2>
-              <p><progress class="progress progress-success w-56" value="70" max="100"></progress></p>
-              </div>
+      //              <div className="">
+      //         <h1  className='text-4xl tracking-tight font-extrabold text-indigo-200  mb-5'> Programming Skill</h1>
+      //         <h2 className='text-2xl font-serif font-bold text-orange-600 '>html</h2>
+      //         <p><progress class="progress progress-success w-56" value="90" max="100"></progress>   </p>
+      //         <h2 className='text-2xl font-serif font-bold text-orange-600'>Css</h2>
+      //         <p><progress class="progress progress-success w-56" value="70" max="100"></progress></p>
+      //         <h2 className='text-2xl font-serif font-bold text-orange-600'>JavaScript</h2>
+      //         <p><progress class="progress progress-success w-56" value="65" max="100"></progress></p>
+      //         <h2 className='text-2xl font-serif font-bold text-orange-600'>React</h2>
+      //         <p><progress class="progress progress-success w-56" value="70" max="100"></progress></p>
+      //         <h2 className='text-2xl font-serif font-bold text-orange-600'>Node Js</h2>
+      //         <p><progress class="progress progress-success w-56" value="60" max="100"></progress></p>
+      //         <h2 className='text-2xl font-serif font-bold text-orange-600'>MongoDb</h2>
+      //         <p><progress class="progress progress-success w-56" value="65" max="100"></progress></p>
+      //         <h2 className='text-2xl font-serif font-bold text-orange-600'>Firebase</h2>
+      //         <p><progress class="progress progress-success w-56" value="70" max="100"></progress></p>
+      //         </div>
        
-      </div>
+      // </div>
         
-                
-        
-      </div>
+      // </div>
     
     );
+
+
+
 };
 
 export default About;
--- src/components/Pages/Home/Projects.js
+++ src/components/Pages/Home/Projects.js
@@ -36,7 +36,7 @@ const products = [
 const Projects = () => {
 
     return (
-        <div className="" id='projects'>
+        <div className="" id='projects' >
         <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
           <h2 className="text-4xl font-extrabold tracking-tight text-orange-600 text-center">Projects</h2>
   
